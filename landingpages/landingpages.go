1d9f55743df14beb51c36e3fcaf8c122226d7147
Douglas Fils <drfils@gmail.com>
2018-03-25 22:14:59 -0500

    more dataset landing page updates....

--- landingpages/landingpages.go
+++ landingpages/landingpages.go
@@ -11,6 +11,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type LPElements struct {
+	So string
+	Rm string
+	Dl string
+}
+
 // Catalog  NOTE the so slice does nothing..  the template is just static content.
 // This function is here only in the thought this could be a dynamic list at some point.
 func Catalog(w http.ResponseWriter, r *http.Request) {
@@ -36,7 +42,12 @@ func LandingPage(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	log.Printf("Landing page for ID for resource: %s\n", vars["id"])
 
-	so := schemaFromPackage(vars["id"]) // TODO..   injection danger?   strings.Trim this...
+	so := contentFromPackage(vars["id"], "schemaorg") // TODO..   injection danger?   strings.Trim this...
+	rm := contentFromPackage(vars["id"], "readme")
+	dl := fmt.Sprintf("/datapackages/%s.zip", vars["id"])
+
+	lpe := LPElements{So: so, Rm: rm, Dl: dl}
+
 	templateFile := "./templates/lptemplate.html"
 
 	ht, err := template.New("Template").ParseFiles(templateFile) //open and parse a template text file
@@ -44,21 +55,22 @@ func LandingPage(w http.ResponseWriter, r *http.Request) {
 		log.Printf("template parse failed: %s", err)
 	}
 
-	err = ht.ExecuteTemplate(w, "Q", so)
+	err = ht.ExecuteTemplate(w, "Q", lpe)
 	if err != nil {
 		log.Printf("Template execution failed: %s", err)
 	}
 }
 
-func schemaFromPackage(datapkg string) string {
+func contentFromPackage(datapkg string, resourcename string) string {
+	log.Print(fmt.Sprintf("./static/datapackages/%s/datapackage.json", datapkg))
 	pkg, err := datapackage.Load(fmt.Sprintf("./static/datapackages/%s/datapackage.json", datapkg))
-	// pkg, err := datapackage.Load("https://geodex.org/id/datafiles/REFID/datapackage.json")
+
 	if err != nil {
 		log.Printf("Error getting package: %v\n", err)
 		return "Error getting package"
 	}
 
-	resource := pkg.GetResource("schemaorg") // could I also read the "schemaorg" JSON file in and use it?
+	resource := pkg.GetResource(resourcename) // could I also read the "schemaorg" JSON file in and use it?
 	rc, err := resource.RawRead()
 	defer rc.Close()
 	contents, _ := ioutil.ReadAll(rc)
--- static/datapackages/p418graph/README.md
+++ static/datapackages/p418graph/README.md
@@ -1,7 +1,5 @@
 ## P418 Graph
 
 ### About
-A file containing the triples generated in the crawling of P418 
-contributing data facilities.  
-
-
+A file containing the triples generated in the crawling of P418
+contributing data facilities.
